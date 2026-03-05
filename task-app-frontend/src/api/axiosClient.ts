06f7acc184bcc75e49f29c2c2ffa0471b410f8fc
Pubudu Ishan <pubudu.ishan@laugfs.lk>
2026-03-05 09:42:36 +0530

    Removed Comment.

--- task-app-frontend/src/api/axiosClient.ts
+++ task-app-frontend/src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:3000', // Adjust to your backend URL
+  baseURL: 'http://localhost:3000',
   headers: {
     'Content-Type': 'application/json',
   },
